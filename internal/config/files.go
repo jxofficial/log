97f753bfaafdea9bd7d120b4cde16505bed1a864
JXKENG <junxian.j.keng@gmail.com>
2022-05-15 15:51:56 +0800

    add tls code to create *tls.Config structs to pass into grpc client and server

---
+++ internal/config/files.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+)
+
+var (
+	CAFile         = configFile("ca.pem")
+	ServerCertFile = configFile("server.pem")
+	ServerKeyFile  = configFile("server-key.pem")
+)
+
+// configFile returns the filepath of TLS / CA related files given a filename.
+func configFile(filename string) string {
+	if dir := os.Getenv("CONFIG_DIR"); dir != "" {
+		return filepath.Join(dir, filename)
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Join(homeDir, ".proglog", filename)
+}
